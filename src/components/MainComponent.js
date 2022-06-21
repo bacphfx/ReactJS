c160026025bb7aa1a4192cc3390be99a932fe6b4
Pham Ho Bac <bacphfx16330@funix.edu.vn>
2022-06-21 16:25:52 +0700

    SPA part 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -32,6 +32,22 @@ class Main extends Component {
         />
       );
     };
+
+    const DishWithID = ({ match }) => {
+      return (
+        <DishDetail
+          dish={
+            this.state.dishes.filter(
+              (dish) => dish.id === parseInt(match.params.dishId, 10)
+            )[0]
+          }
+          commnet={this.state.comments.filter(
+            (comment) => comment.dishId === parseInt(match.params.dishId, 10)
+          )}
+        />
+      );
+    };
+
     return (
       <div>
         <Header />
@@ -42,6 +58,7 @@ class Main extends Component {
             path="/menu"
             component={() => <Menu dishes={this.state.dishes} />}
           />
+          <Route path='/menu/"dishID' component={DishWithID} />
           <Route exact path="/contactus" component={Contact} />
           <Redirect to="/home" />
         </Switch>
