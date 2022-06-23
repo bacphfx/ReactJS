cd76ecf228874a543165e920e7409f4bb45f1a52
Pham Ho Bac <bacphfx16330@funix.edu.vn>
2022-06-23 10:29:54 +0700

    asm2 done

--- src/components/SalaryComponent.js
+++ src/components/SalaryComponent.js
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { Component } from "react";
 import {
   Card,
   CardHeader,
   CardText,
   Breadcrumb,
   BreadcrumbItem,
+  Form,
+  Button,
 } from "reactstrap";
 import { Link } from "react-router-dom";
 
+// tạo function hiển thị thông tin bảng lương của nhân nhiên
 function RenderSalary({ staff }) {
+  const salaryCalc = staff.salaryScale * 3000000 + staff.overTime * 200000;
+  staff.salaryCalc = salaryCalc;
   return (
     <Card>
       <h3>{staff.name}</h3>
@@ -17,28 +22,25 @@ function RenderSalary({ staff }) {
         <p>Hệ số lương: {staff.salaryScale}</p>
         <p>Số ngày làm thêm: {staff.overTime}</p>
       </CardHeader>
-      <CardText className="ml-4">
-        Lương: {staff.salaryScale * 3000000 + staff.overTime * 200000}
-      </CardText>
+      <CardText className="ml-4">Lương: {staff.salaryCalc}</CardText>
     </Card>
   );
 }
 
-const Salary = (props) => {
-  //   const salary = props.staffs.map((staff) => {
-  //     <div key={staff.id} className="col-6 col-sm-6 col-md-4 mt-3">
-  //       <RenderSalary staff={staff} />
-  //     </div>;
-  //   });
-  const salary = props.staffs.map((staff) => {
+// Khai báo component Salary
+class Salary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { sort: "id uppering" };
+    this.renderBreedcrum = this.renderBreedcrum.bind(this);
+  }
+  onSortSelect = function (sort) {
+    this.setState({ sort: sort });
+  };
+
+  // function hiển thị Breadcrumb, dùng nhiều lần
+  renderBreedcrum() {
     return (
-      <div key={staff.id} className="col-12 col-sm-6 col-md-4 mt-3">
-        <RenderSalary staff={staff} />
-      </div>
-    );
-  });
-  return (
-    <div className="container">
       <div className="row">
         <Breadcrumb>
           <BreadcrumbItem>
@@ -46,10 +48,148 @@ const Salary = (props) => {
           </BreadcrumbItem>
           <BreadcrumbItem active>Bảng lương</BreadcrumbItem>
         </Breadcrumb>
+        <div className="container">
+          <Form className="pull-right">
+            <Button
+              className="btn btn-primary pull-right mr-1"
+              onClick={() => this.onSortSelect("salary lowering")}
+            >
+              Lương ↓
+            </Button>
+            <Button
+              className="btn btn-primary pull-righ mr-1"
+              onClick={() => this.onSortSelect("salary uppering")}
+            >
+              Lương ↑
+            </Button>
+            <Button
+              className="btn btn-primary pull-right mr-1"
+              onClick={() => this.onSortSelect("id lowering")}
+            >
+              ID ↓
+            </Button>
+            <Button
+              className="btn btn-primary pull-right mr-1"
+              onClick={() => this.onSortSelect("id uppering")}
+            >
+              ID ↑
+            </Button>
+          </Form>
+        </div>
       </div>
-      <div className="row mb-5">{salary}</div>
-    </div>
-  );
-};
+    );
+  }
+
+  render() {
+    // Nếu state là id tăng dần
+    if (this.state.sort === "id uppering") {
+      // Tạo mảng mới theo id tăng dần
+      const sortByIDUperring = this.props.staffs.sort(function (a, b) {
+        if (a.id < b.id) {
+          return -1;
+        }
+        if (a.id > b.id) {
+          return 1;
+        }
+        return 0;
+      });
+      // render mảng mới có id tăng dần
+      const salary = sortByIDUperring.map((staff) => {
+        return (
+          <div key={staff.id} className="col-12 col-sm-6 col-md-4 mt-3">
+            <RenderSalary staff={staff} />
+          </div>
+        );
+      });
+      return (
+        <div className="container">
+          <this.renderBreedcrum />
+          <div className="row mb-5">{salary}</div>
+        </div>
+      );
+    }
+    // Nếu state là id giảm dần
+    else if (this.state.sort === "id lowering") {
+      // tạo mảng mới theo id giảm dần
+      const sortByIDLowering = this.props.staffs.sort(function (a, b) {
+        if (a.id > b.id) {
+          return -1;
+        }
+        if (a.id < b.id) {
+          return 1;
+        }
+        return 0;
+      });
+      // render mảng mới có id giảm dần
+      const salary = sortByIDLowering.map((staff) => {
+        return (
+          <div key={staff.id} className="col-12 col-sm-6 col-md-4 mt-3">
+            <RenderSalary staff={staff} />
+          </div>
+        );
+      });
+      return (
+        <div className="container">
+          <this.renderBreedcrum />
+          <div className="row mb-5">{salary}</div>
+        </div>
+      );
+    }
+    // Nếu state là lương tăng dần
+    else if (this.state.sort === "salary uppering") {
+      // Tạo mảng mới theo lương tăng dần
+      const sortBySalaryUppering = this.props.staffs.sort(function (a, b) {
+        if (a.salaryCalc < b.salaryCalc) {
+          return -1;
+        }
+        if (a.salaryCalc > b.salaryCalc) {
+          return 1;
+        }
+        return 0;
+      });
+      // render mảng mới có lương tăng dần
+      const salary = sortBySalaryUppering.map((staff) => {
+        return (
+          <div key={staff.id} className="col-12 col-sm-6 col-md-4 mt-3">
+            <RenderSalary staff={staff} />
+          </div>
+        );
+      });
+      return (
+        <div className="container">
+          <this.renderBreedcrum />
+          <div className="row mb-5">{salary}</div>
+        </div>
+      );
+    }
+    // Nếu state là lương giảm dần
+    else if (this.state.sort === "salary lowering") {
+      // Tạo mảng mới có lương giảm dần
+      const sortBySalaryLowering = this.props.staffs.sort(function (a, b) {
+        if (a.salaryCalc > b.salaryCalc) {
+          return -1;
+        }
+        if (a.salaryCalc < b.salaryCalc) {
+          return 1;
+        }
+        return 0;
+      });
+      // render mảng mới có lương giảm dần
+      const salary = sortBySalaryLowering.map((staff) => {
+        return (
+          <div key={staff.id} className="col-12 col-sm-6 col-md-4 mt-3">
+            <RenderSalary staff={staff} />
+          </div>
+        );
+      });
+      return (
+        <div className="container">
+          <this.renderBreedcrum />
+          <div className="row mb-5">{salary}</div>
+        </div>
+      );
+    }
+  }
+}
 
 export default Salary;
--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -12,6 +12,7 @@ import {
 } from "reactstrap";
 import { Link } from "react-router-dom";
 
+// tạo function hiển thị nhân viên
 function RenderStaffList({ staff }) {
   return (
     <Card>
@@ -23,6 +24,7 @@ function RenderStaffList({ staff }) {
   );
 }
 
+// Khai báo component StaffList
 class StaffList extends Component {
   constructor(props) {
     super(props);
@@ -30,13 +32,46 @@ class StaffList extends Component {
       keyword: "",
       staffSearched: null,
     };
+    this.renderBreedcrum = this.renderBreedcrum.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
+  // function hiển thị Breadcrumb, dùng nhiều lần
+  renderBreedcrum() {
+    return (
+      <div className="row">
+        <Breadcrumb>
+          <BreadcrumbItem>
+            <Link to="/staffs"></Link>
+          </BreadcrumbItem>
+          <BreadcrumbItem active>Nhân viên</BreadcrumbItem>
+        </Breadcrumb>
+        <div className="container">
+          <Form className="pull-right" onSubmit={this.handleSubmit}>
+            <Button className="btn btn-primary pull-right">Seacrh</Button>
+            <FormGroup className="d-inline pull-right">
+              <Input
+                type="text"
+                value={this.state.value}
+                onChange={this.handleChange}
+                placeholder="Nhập từ khóa để tìm kiếm"
+              />
+            </FormGroup>
+          </Form>
+        </div>
+        <div className="col-12">
+          <h3>Nhân viên</h3>
+        </div>
+      </div>
+    );
+  }
+
+  // Hàm lấy keyword người dùng nhập vào
   handleChange(event) {
     this.setState({ keyword: event.target.value });
   }
 
+  // Hàm tìm nhân viên thỏa mãn keyword
   handleSubmit(event, searchResult) {
     event.preventDefault();
     searchResult = this.props.staffs.filter((staff) =>
@@ -51,6 +86,7 @@ class StaffList extends Component {
     );
   }
   render() {
+    // Nếu chưa thực hiện tìm kiếm
     if (this.state.staffSearched == null) {
       const staffList = this.props.staffs.map((staff) => {
         return (
@@ -62,35 +98,14 @@ class StaffList extends Component {
 
       return (
         <div className="container">
-          <div className="row">
-            <Breadcrumb>
-              <BreadcrumbItem>
-                <Link to="/staffs"></Link>
-              </BreadcrumbItem>
-              <BreadcrumbItem active>Nhân viên</BreadcrumbItem>
-            </Breadcrumb>
-            <div className="container">
-              <Form className="pull-right" onSubmit={this.handleSubmit}>
-                <Button className="btn btn-primary pull-right">Seacrh</Button>
-                <FormGroup className="d-inline pull-right">
-                  <Input
-                    type="text"
-                    value={this.state.value}
-                    onChange={this.handleChange}
-                    placeholder="Nhập từ khóa để tìm kiếm"
-                  />
-                </FormGroup>
-              </Form>
-            </div>
-            <div className="col-12">
-              <h3>Nhân viên</h3>
-            </div>
-          </div>
-
+          <this.renderBreedcrum />
           <div className="row mb-5">{staffList}</div>
         </div>
       );
-    } else {
+    }
+    // Nếu thực hiện tìm kiếm
+    else {
+      // Tạo mảng nhân viên từ state staffListSearched
       const staffListSearched = this.state.staffSearched.map(
         (staffSearched) => {
           return (
@@ -105,30 +120,7 @@ class StaffList extends Component {
       );
       return (
         <div className="container">
-          <div className="row">
-            <Breadcrumb>
-              <BreadcrumbItem>
-                <Link to="/staffs"></Link>
-              </BreadcrumbItem>
-              <BreadcrumbItem active>Nhân viên</BreadcrumbItem>
-            </Breadcrumb>
-            <div className="container">
-              <Form className="pull-right" onSubmit={this.handleSubmit}>
-                <Button className="btn btn-primary pull-right">Seacrh</Button>
-                <FormGroup className="d-inline pull-right">
-                  <Input
-                    type="text"
-                    value={this.state.value}
-                    onChange={this.handleChange}
-                    placeholder="Nhập từ khóa để tìm kiếm"
-                  />
-                </FormGroup>
-              </Form>
-            </div>
-            <div className="col-12">
-              <h3>Nhân viên</h3>
-            </div>
-          </div>
+          <this.renderBreedcrum />
           <div className="row mb-5">{staffListSearched}</div>
         </div>
       );
