88a4e0ab15daca89b2f3c38377bd2f5aa829cb76
Pham Ho Bac <bacphfx16330@funix.edu.vn>
2022-07-10 23:58:06 +0700

    add animation

--- src/components/StaffDetailComponent.js
+++ src/components/StaffDetailComponent.js
@@ -3,27 +3,35 @@ import { Media, Breadcrumb, BreadcrumbItem } from "reactstrap";
 import dateFormat from "dateformat";
 import { Link } from "react-router-dom";
 import { Loading } from "./LoadingComponents";
+import { FadeTransform } from "react-animation-components";
 
 function RenderStaffDetail({ staff }) {
   return (
-    <div key={staff.id}>
-      <Media tag="li">
-        <img
-          className="col-12 col-sm-4 col-md-3"
-          src={staff.image}
-          alt={staff.name}
-        />
+    <FadeTransform
+      in
+      transformProps={{
+        exitTransform: "scale(0.5) translateY(-50%)",
+      }}
+    >
+      <div key={staff.id}>
+        <Media tag="li">
+          <img
+            className="col-12 col-sm-4 col-md-3"
+            src={staff.image}
+            alt={staff.name}
+          />
 
-        <Media body className="col-12 col-sm-8 col-md-9 ml-1">
-          <Media heading>Họ và tên: {staff.name}</Media>
-          <p>Ngày sinh: {dateFormat(staff.doB, "dd/mm/yyyy")}</p>
-          <p>Ngày vào công ty: {dateFormat(staff.startDate, "dd/mm/yyyy")}</p>
-          <p>Phòng ban: {staff.department}</p>
-          <p>Số ngày nghỉ còn lại: {staff.annualLeave}</p>
-          <p>Số ngày làm thêm: {staff.overTime}</p>
+          <Media body className="col-12 col-sm-8 col-md-9 ml-1">
+            <Media heading>Họ và tên: {staff.name}</Media>
+            <p>Ngày sinh: {dateFormat(staff.doB, "dd/mm/yyyy")}</p>
+            <p>Ngày vào công ty: {dateFormat(staff.startDate, "dd/mm/yyyy")}</p>
+            <p>Phòng ban: {staff.department}</p>
+            <p>Số ngày nghỉ còn lại: {staff.annualLeave}</p>
+            <p>Số ngày làm thêm: {staff.overTime}</p>
+          </Media>
         </Media>
-      </Media>
-    </div>
+      </div>
+    </FadeTransform>
   );
 }
 
